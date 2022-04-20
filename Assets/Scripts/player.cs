464bd4a4476dd1e426388f38479e88df013f8ca3
KnivesPeans <knivespeans@gmail.com>
2022-04-19 20:59:18 -0500

    Update player.cs

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    int Speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Speed = 
     }
 
     // Update is called once per frame
