9fba38b3ccd9e347b4ce3b0ed72d1b0f1d301aea
KnivesPeans <knivespeans@gmail.com>
2022-05-24 22:32:42 -0500

    Add Inventary

---
+++ Assets/Scripts/Inventary.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventary : MonoBehaviour
+{
+
+    public PlayerController playerController;
+    public List<GameObject> objects = new List<GameObject>();
+    public GameObject inv;
+    public Image invImage;
+    public bool activeInv;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color c = invImage.color;
+        c.a = 0f;
+        invImage.color = c;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            activeInv = !activeInv;
+        }
+        if (Input.GetKeyDown(KeyCode.Return) && activeInv)
+        {
+            startFading();
+        }
+        if (Input.GetKeyDown(KeyCode.Return) && !activeInv)
+        {
+            startFadeOut();
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Item"))
+        {
+            for (int i = 0; i < objects.Count; i++)
+            {
+                if (objects[i].GetComponent<Image>().enabled == false)
+                {
+                    playerController.haveKey = true;
+                    objects[i].GetComponent<Image>().enabled = true;
+                    objects[i].GetComponent<Image>().sprite = coll.GetComponent<Image>().sprite;
+
+                    if (activeInv)
+                    {
+                        starfadingkey();
+                    }
+                    if (!activeInv)
+                    {
+                        Color c = objects[i].GetComponent<Image>().color;
+                        c.a = 0f;
+                        objects[i].GetComponent<Image>().color = c;
+                    }
+
+                    Destroy(coll.gameObject);
+                    break;
+
+                }
+            }
+        }
+
+        if (coll.CompareTag("Door") && playerController.haveKey)
+        {
+            objects[0].GetComponent<Image>().sprite = null;
+            objects[0].GetComponent<Image>().enabled = false;
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            if (objects[0].GetComponent<Image>().enabled)
+            {
+                Color d = objects[0].GetComponent<Image>().color;
+                d.a = f;
+                objects[0].GetComponent<Image>().color = d;
+
+            }
+            Color c = invImage.color;
+            c.a = f;
+            invImage.color = c;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            if (objects[0].GetComponent<Image>().enabled)
+            {
+                Color d = objects[0].GetComponent<Image>().color;
+                d.a = f;
+                objects[0].GetComponent<Image>().color = d;
+
+            }
+            Color c = invImage.color;
+            c.a = f;
+            invImage.color = c;
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+    IEnumerator FadeInkey()
+    {
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color d = objects[0].GetComponent<Image>().color;
+            d.a = f;
+            objects[0].GetComponent<Image>().color = d;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+
+    }
+    public void startFading()
+    {
+        StartCoroutine("FadeIn");
+    }
+    public void startFadeOut()
+    {
+        StartCoroutine("FadeOut");
+    }
+    public void starfadingkey()
+    {
+        StartCoroutine("FadeInkey");
+    }
+}
+
