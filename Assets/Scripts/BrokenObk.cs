21ec3ace3df2bd9dc89420acb30e6e8b4ff825aa
LuisZapata12 <luis.98.30.16@gmail.com>
2022-06-03 11:25:20 -0500

    VaseBroken

---
+++ Assets/Scripts/BrokenObk.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenObk : MonoBehaviour
+{
+    public MeshFilter vase;
+    public MeshFilter vaseBroken;
+    public bool Broken;
+    public GameObject key;
+    public float timer;
+    public float MaxTimer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      /*  if (Broken)
+        {
+            timer += Time.deltaTime;
+            if (timer > MaxTimer)
+            {
+                KeyCreate();
+                Broken
+            }
+            
+        }*/
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            vase.GetComponent<MeshFilter>();
+            vase.mesh = vaseBroken.mesh;
+            Broken = true;
+            KeyCreate();
+        }
+    }
+    public void KeyCreate()
+    {
+        GameObject KeyDrop;
+        KeyDrop = Instantiate(key, transform.position, Quaternion.identity);
+
+    }
+}
