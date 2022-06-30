4e018bcea2b5e143534ed8b0a9a4246048be1dc5
LuisZapata12 <luis.98.30.16@gmail.com>
2022-06-29 22:58:29 -0500

    UpdateTuto

--- Assets/Scripts/DialogText.cs
+++ Assets/Scripts/DialogText.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class DialogText : MonoBehaviour
 {
     public GameObject Text;
+    public GameObject TextDialog;
+    public bool Action1;
+    public bool Action2;
+    
     void Start()
     {
         
@@ -13,14 +17,33 @@ public class DialogText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Manager.isPause)
+        {
+            Text.SetActive(false);
+            TextDialog.SetActive(false);
+        }
+        if (Action1)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Action2 = true;
+                Action1 = false;
+            }
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Text.SetActive(true);
+    
+            if (Action2)
+            {
+                Text.SetActive(false);
+                TextDialog.SetActive(true);
+            }
+
         }
         
     }
@@ -29,6 +52,9 @@ public class DialogText : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Text.SetActive(false);
+            TextDialog.SetActive(false);
+            Action1 = true;
+            Action2 = false;
         }
     }
 }
