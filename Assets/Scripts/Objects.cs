adb8f6c81c5804005d02afc0b3fb4a7bb54b87d5
jorgeluis15520 <103298795+jorgeluis15520@users.noreply.github.com>
2022-07-03 03:09:04 -0500

    Font Fix

--- Assets/Scripts/Objects.cs
+++ Assets/Scripts/Objects.cs
@@ -4,20 +4,59 @@ using UnityEngine;
 
 public class Objects : MonoBehaviour
 {
-    PlayerController player;
-
+    public List<GameObject> waterParts = new List<GameObject>();
+    public GameObject efectSound;
     public bool active;
 
     public AudioClip clip;
     public AudioSource audioSource;
+    private AudioSource fontAudio;
+    private bool inArea = false;
+    private bool end = false;
+    public float timer;
+    private int currentPart = 0;
 
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
+    {
+        fontAudio = efectSound.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (inArea && Input.GetKeyDown(KeyCode.E) && !active)
+        {
+            audioSource.PlayOneShot(clip);
+            fontAudio.Play();
+            active = true;
+            StartCoroutine(WaterActive());
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
-            active = true;
+            inArea = true;
+        }
+    }
 
-            audioSource.PlayOneShot(clip);
+    private IEnumerator WaterActive()
+    {
+        if (end)
+        {
+            yield break;
+        }
+
+        for (int i = 0; i < waterParts.Count; i++)
+        {
+            currentPart = i;
+            waterParts[currentPart].SetActive(true);
+            yield return new WaitForSeconds(timer);
+        }
+
+        if (currentPart >= waterParts.Count)
+        {
+            end = true;
         }
     }
 }
